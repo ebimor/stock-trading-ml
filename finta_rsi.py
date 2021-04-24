07eedb2b67c10063008a4628a1f5f23ada2d6a0c
Ebrahim Shahrivar <eshahrivar@clearpath.ai>
2021-04-24 19:47:08 -0400

    plot and calc rsi

--- finta_rsi.py
+++ finta_rsi.py
@@ -2,11 +2,13 @@ import pandas as pd
 from finta import TA
 import matplotlib.pyplot as plt
 import mplfinance as mpf
+from pandas_datareader import data as web
 
 
+#data = pd.read_csv("SPY_daily_finata.csv", header=0, index_col="Date", parse_dates=True)
+data = web.DataReader('^SPX', 'stooq')
+data.columns = ["open", "high", "low", "close", "volume"]
 
-
-data = pd.read_csv("SPY_daily_finata.csv", header=0, index_col="Date", parse_dates=True)
 #ohlc.columns = ["Date", "open", "high", "low", "close", "volume"]
 data = data.iloc[::-1]
 
@@ -16,7 +18,7 @@ data.to_csv('withRSI.csv')
 
 #ohlc_plot = pd.concat([bbands.BB_UPPER, bbands.BB_LOWER], axis=1)
 
-apd = mpf.make_addplot(data['RSI'].tail(300), color='g',panel=2)
+apd = mpf.make_addplot(data['RSI'].tail(365), color='g',panel=2, ylabel="RSI")
 
 mpf.plot(data.tail(300), type='candle', style='charles',
         title='SPY',
