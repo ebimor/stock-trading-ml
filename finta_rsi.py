9052129ebe252bd625eb80966f362b8385fa41f2
Ebrahim Shahrivar <eshahrivar@clearpath.ai>
2021-05-01 20:45:24 -0400

    wip get data from yahoo

--- finta_rsi.py
+++ finta_rsi.py
@@ -3,11 +3,15 @@ from finta import TA
 import matplotlib.pyplot as plt
 import mplfinance as mpf
 from pandas_datareader import data as web
+from datetime import datetime
 
 
-#data = pd.read_csv("SPY_daily_finata.csv", header=0, index_col="Date", parse_dates=True)
-data = web.DataReader('^SPX', 'stooq')
-data.columns = ["open", "high", "low", "close", "volume"]
+end = datetime.now()
+start = datetime(end.year - 1, end.month, end.day)
+
+
+data = web.DataReader('^SPX', 'google', start, end)
+#data.columns = ["High", "Low", "Open", "Close", "Volume", ""]
 
 #ohlc.columns = ["Date", "open", "high", "low", "close", "volume"]
 data = data.iloc[::-1]
@@ -18,7 +22,7 @@ data.to_csv('withRSI.csv')
 
 #ohlc_plot = pd.concat([bbands.BB_UPPER, bbands.BB_LOWER], axis=1)
 
-apd = mpf.make_addplot(data['RSI'].tail(365), color='g',panel=2, ylabel="RSI")
+#apd = mpf.make_addplot(data['RSI'].tail(300), color='g',panel=2, ylabel="RSI")
 
 mpf.plot(data.tail(300), type='candle', style='charles',
         title='SPY',
@@ -26,5 +30,4 @@ mpf.plot(data.tail(300), type='candle', style='charles',
         ylabel_lower='Volume',
         volume=True,
         figscale=1.5,
-        addplot=apd
         )
